ee5814dbfbf43953652c59ad1e7b8567ed268503
Arnold Kamminga <arnold.kamminga@hotmail.com>
2023-02-14 08:49:37 +0100

    force minimum update interval of 60

--- plugin.py
+++ plugin.py
@@ -540,6 +540,9 @@ class BasePlugin:
         vocpass=Parameters["Password"]
         vccapikey=Parameters["Mode1"]
         updateinterval=int(Parameters["Mode2"])
+        if (updateinterval<60):
+            Info("Updateinterval too low, correcting to 60 secs")
+            updateinterval=59 # putting is too exact 60 might sometimes lead to update after 70 secs 
         lastupdate=time.time()-updateinterval-1 #force update
         expirytimestamp=time.time()-1 #force update
 
