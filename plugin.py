f9d16575921f5c870a44903f312302796efc08ab
Arnold Kamminga <arnold.kamminga@hotmail.com>
2023-02-15 20:16:30 +0100

    manually configurable VIN

--- plugin.py
+++ plugin.py
@@ -19,6 +19,7 @@
         <ul style="list-style-type:square">
             <li>Use your Volvo on Call Username/password, which is linked to your vehicle.</li>
             <li>Register an app on https://developer.volvocars.com/apis/docs/getting-started/ and copy/past the primary app key in the config below</li>
+            <li>Optional: Set a VIN if you connected more than one car to your volvo account. If empty the plugin will use the 1st car attached to your Volvo account</li>
             <li>Set an update interval. If you don't pay Volvo for the API, you're only allowed to do 10.000 calls per day.. so make sure not to set the update interval too high. The plugin does several calles per interval.</li>
         </ul>
     </description>
@@ -27,6 +28,7 @@
         <param field="Password" label="Volvo On Call Password" required="true" password="true"/>
         <param field="Mode1" label="Primary VCC API Key" required="true"/>
         <param field="Mode2" label="update interval in secs" required="true" default="900"/>
+        <param field="Mode3" label="VIN (optional)"/>
         <param field="Mode6" label="Debug" width="150px">
             <options>
                 <option label="None" value="0"  default="true" />
@@ -179,30 +181,34 @@ def GetVin():
     global vin
 
     Debug("GetVin called")
+    if len(Parameters["Mode3"])==0:
+        Debug("No Vin entered in Config, try getting one from Volvo account")
+        try:
+            vehicles = requests.get(
+                "https://api.volvocars.com/extended-vehicle/v1/vehicles",
+                headers= {
+                    "accept": "application/json",
+                    "vcc-api-key": vccapikey,
+                    "Authorization": "Bearer " + access_token
+                }
+            )
+            Debug("\nResult:")
+            Debug(vehicles)
+            vjson=vehicles.json()
 
-    try:
-        vehicles = requests.get(
-            "https://api.volvocars.com/extended-vehicle/v1/vehicles",
-            headers= {
-                "accept": "application/json",
-                "vcc-api-key": vccapikey,
-                "Authorization": "Bearer " + access_token
-            }
-        )
-        Debug("\nResult:")
-        Debug(vehicles)
-        vjson=vehicles.json()
+            vehiclesjson = json.dumps(vehicles.json(), indent=4)
+            Debug("\nResult JSON:")
+            Debug(vehiclesjson)
+            vin = vjson["vehicles"][0]["id"]
+            Info("Retreived Vehicle VIN: "+vin)
 
-        vehiclesjson = json.dumps(vehicles.json(), indent=4)
-        Debug("\nResult JSON:")
-        Debug(vehiclesjson)
-        vin = vjson["vehicles"][0]["id"]
-        Info("Retreived Vehicle VIN: "+vin)
-
-    except requests.exceptions.RequestException as error:
-        Debug("Get vehicles failed:")
-        Debug(error)
-        vin=None
+        except requests.exceptions.RequestException as error:
+            Debug("Get vehicles failed:")
+            Debug(error)
+            vin=None
+    else:
+        vin=Parameters["Mode3"]
+        Info("Using configured VIN "+str(vin))
 
 
 def VolvoAPI(url,mediatype):
